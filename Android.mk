8e96b349dd434bb34bb85b12f6ffada75fa1a049
Ayush <ayush@disroot.org>
2020-12-06 12:35:23 +0530

    RMX1831: add ImsService
    * compiled from alps pie.
    * modified version of https://treble.phh.me/stable/ims-mtk-p.apk
      with fixes for outgoing message.
    
    Co-authored-by: Pierre-Hugues Husson <phh@phh.me>
    Signed-off-by: Ayush <ayush@disroot.org>
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,4 +18,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1831)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := ImsService
+LOCAL_MODULE_OWNER := oppo
+LOCAL_SRC_FILES := proprietary/priv-app/ImsService/ImsService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
