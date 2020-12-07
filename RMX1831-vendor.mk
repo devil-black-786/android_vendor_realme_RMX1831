5db43e3df98272d19e5473a3421b685c832ec4c0
SparXFusion <s2234nadar@gmail.com>
2020-12-08 03:36:43 +0530

    RMX1831-vendor: nuke libmtk-ril for now
    * We are using OSS variant of ril now
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- RMX1831-vendor.mk
+++ RMX1831-vendor.mk
@@ -6,5 +6,4 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1831/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/RMX1831/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1831/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
-    vendor/realme/RMX1831/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so \
     vendor/realme/RMX1831/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so 
